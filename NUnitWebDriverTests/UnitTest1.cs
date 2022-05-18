bc8c31a01fe0580385460a26084e09c5e14c8d12
user <user@WIN-FD0BI7PB7LC>
2022-05-18 16:29:53 +0100

    Add project files.

---
+++ NUnitWebDriverTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace NUnitWebDriverTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var driver = new ChromeDriver();
+            driver.Url =
+            Assert.Pass();
+        }
+    }
+}
---
+++ WebDriverTestDemo/WikiPedia.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Create new Chrome Instance
+var driver = new ChromeDriver();
+
+//Navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+Console.WriteLine("CURRENT TITLE:" + driver.Title);
+
+//Locate SearchField by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+//Click on element 
+searchField.Click();
+
+searchField.SendKeys("QA" + Keys.Enter);
+//Close Browser
+//driver.Quit();
+Console.WriteLine("TITLE AFTER SEARCH: " +driver.Title);
